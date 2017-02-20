fecb9d505d416147392c59d47690d64cfa7bff5c
AznBoy00 <Lin_Kevin_1995@hotmail.com>
2017-02-20 01:38:00 -0500

    P1 done.

---
+++ A2/Part 1/InformationDesk/PublicTransportation.java
@@ -0,0 +1,98 @@
+/*
+    Assignment #2
+    Part: PublicTransportation
+    Written by: Kevin Lin - 40002383
+*/
+
+package InformationDesk;
+
+/**
+ *
+ * @author Kevin
+ */
+public class PublicTransportation {
+    protected double ticketPrice;
+    protected int numOfStops;
+    
+    /**
+     * Default Constructor
+     */
+    public PublicTransportation() {
+    }
+    /**
+     * Copy Constructor
+     * @param pt PublicTransportation Object
+     */
+    public PublicTransportation(PublicTransportation pt) {
+        this.ticketPrice = pt.ticketPrice;
+        this.numOfStops = pt.numOfStops;
+    }
+    /**
+     * Constructor taking ticketPrice and numOfStops
+     * @param ticketPrice ticket price
+     * @param numOfStops number of stops
+     */
+    public PublicTransportation(double ticketPrice, int numOfStops) {
+        this.ticketPrice = ticketPrice;
+        this.numOfStops = numOfStops;
+    }
+    /**
+     * Getter for ticketPrice
+     * @return price of ticket.
+     */
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+    /**
+     * Setter for ticketPrice
+     * @param ticketPrice price of ticket.
+     */
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+    /**
+     * Getter for numOfStops.
+     * @return number of stops.
+     */
+    public int getNumOfStops() {
+        return numOfStops;
+    }
+    /**
+     * Setter for numOfStops
+     * @param numOfStops number of stops.
+     */
+    public void setNumOfStops(int numOfStops) {
+        this.numOfStops = numOfStops;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PublicTransportation other = (PublicTransportation) obj;
+        
+        if (Double.doubleToLongBits(this.ticketPrice) != Double.doubleToLongBits(other.ticketPrice)) {
+            return false;
+        }
+        if (this.numOfStops != other.numOfStops) {
+            return false;
+        }
+        return true;
+        }
+
+    /**
+     * String representation for this class.
+     * @return description of the class.
+     */
+    @Override
+    public String toString() {
+        return "This PublicTransportation has " + this.numOfStops + " stops, and costs " + this.ticketPrice + "$.";
+    }
+}
---
+++ A2/Part 1/WaterTransportation/Ferry.java
@@ -0,0 +1,69 @@
+/*
+    Assignment #2
+    Part: Ferry
+    Written by: Kevin Lin - 40002383
+*/
+
+package WaterTransportation;
+
+import InformationDesk.PublicTransportation;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Ferry extends PublicTransportation{
+    private int buildYear;
+    
+    public Ferry(Ferry f) {
+        super(f.ticketPrice, f.numOfStops);
+        this.buildYear = f.buildYear;
+    }
+
+    public Ferry(int buildYear, double ticketPrice, int numOfStops) {
+        super(ticketPrice, numOfStops);
+        this.buildYear = buildYear;
+    }
+
+    public Ferry() {
+        super();
+    }
+    
+    public int getBuildYear() {
+        return buildYear;
+    }
+
+    public void setBuildYear(int buildYear) {
+        this.buildYear = buildYear;
+    }
+    
+    @Override
+    public String toString() {
+        return "This Ferry has " + this.numOfStops + " stops, and costs " + this.ticketPrice + "$. The ferry was built in " + this.buildYear + ".";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ferry other = (Ferry) obj;
+        
+        if (Double.doubleToLongBits(this.ticketPrice) != Double.doubleToLongBits(other.ticketPrice)) {
+            return false;
+        }
+        if (this.numOfStops != other.numOfStops) {
+            return false;
+        }
+        if (this.buildYear != other.buildYear) {
+            return false;
+        }
+        return true;
+    } 
+}
